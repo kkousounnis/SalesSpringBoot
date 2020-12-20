a595f91a05bdae6d9a3fd9e4e1c7dbe3e1cc3436
Konstantinos <kwstas654321@gmail.com>
2020-12-20 22:28:02 +0200

    Edit New Product

--- ProductManager/src/main/java/net/codejava/product/controllers/ProductController.java
+++ ProductManager/src/main/java/net/codejava/product/controllers/ProductController.java
@@ -6,7 +6,10 @@ import net.codejava.product.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProductController {
@@ -22,5 +25,33 @@ public class ProductController {
         model.addAttribute("products", products);
         return "listproducts";
     }
+    
+    @RequestMapping("/new")
+    public String showNewProductPage(Model model){
+        Product product = new Product();
+        model.addAttribute("product", product);
+        
+        return "newproduct";
+    }
+    
+    @RequestMapping("/save")
+    public String saveProduct(@ModelAttribute("product") Product product){
+        service.save(product);
+        
+        return "redirect:/";
+    }
+    
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductPage(@PathVariable( name = "id") int id){
+        ModelAndView mav = new ModelAndView("edit_product");
+        Product product = service.get(id);
+        
+        mav.addObject("product", product);
+        
+        return mav;
+    }
+    
+    
+    
 
 }
